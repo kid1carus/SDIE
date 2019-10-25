5f4561825b05c5ae908ca2dab605c68cbfa94740
FutureFlash <50788891+laithayoub71@users.noreply.github.com>
2019-10-24 18:24:10 -0600

    Add files via upload

--- Tools/ENTER-Recovery.bat
+++ Tools/ENTER-Recovery.bat
@@ -1,15 +1,5 @@
-@echo off
-title ENTER Recovery (A5-A13) by FutureFlash
-color 0a
-cd..
-cd src
-set udid=ideviceinfo
->udid.txt (
-%udid%
-findstr /R UniqueDeviceID: udid.txt > udidoutput.txt
-)
-set /p udid1=<udidoutput.txt
+@echo off & title ENTER Recovery (A5-A13) by FutureFlash & color 0a & cd.. & cd src & set udid=ideviceinfo
+%udid%>udid.txt && findstr /R UniqueDeviceID: udid.txt > udidoutput.txt && set /p udid1=<udidoutput.txt
 set udidresult=%udid1:~16,40%
 ideviceenterrecovery %udidresult%
-del udid.txt
-del udidoutput.txt
+del /q /f *.txt
--- Tools/EXIT-Recovery.bat
+++ Tools/EXIT-Recovery.bat
@@ -1,6 +1,1 @@
-@echo off
-title EXIT Recovery (A5-A13) by FutureFlash
-color 0a
-cd..
-cd src
-irecovery -n
+@echo off & title EXIT Recovery (A5-A13) by FutureFlash & color 0a & cd.. && cd src && irecovery -n
